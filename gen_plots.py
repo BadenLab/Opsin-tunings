1410e96c856605d78840cf6e6f08a501f1258282
Simen Bruøygard <simen.bruoy@gmail.com>
2024-06-11 20:59:44 +0100

    Fixed a bounding box issue in plot

--- gen_plots.py
+++ gen_plots.py
@@ -12,7 +12,7 @@ led_wvs = [365, 420, 470, 588]
 colours = np.flip(["#ef8e00", "teal","#5600fe", "fuchsia"])
 ops_name = ["SWS1", "SWS2", "RH2", "LWS1"]
 
-# Plot heatmap
+#Plot heatmap
 fig, ax = plt.subplots(1, 1, figsize = (8, 4), dpi = 200)
 plt.imshow(opsins, cmap = "Greys_r")
 for n, i in enumerate(zeb_ops):
@@ -32,13 +32,11 @@ for n, i in enumerate(zeb_ops):
     line = opsins[i]
     ysmoothed = scipy.ndimage.filters.gaussian_filter1d(line, sigma=2)   
     plt.plot(ysmoothed, c =colours[n], label = f"{ops_name[n]}")
-    # plt.axvspan(led_wvs[n], led_wvs[n]+10, color = colours[n], alpha = .33, lw = 0, label = f"LED {led_wvs[n]}")
 plt.xlim(250, 700)
 plt.xlabel("Wavelength (nm)")
 plt.ylabel("Rel. absorption")
 for n, i in enumerate(zeb_ops):
     plt.axvspan(led_wvs[n], led_wvs[n]+10, color = colours[n], alpha = .33, lw = 0, label = f"LED {led_wvs[n]}")
-plt.tight_layout()
 
 legs_labels = plt.gca().get_legend_handles_labels()
 opsin_labels = legs_labels[1][:4]
@@ -46,8 +44,9 @@ opsin_handles = legs_labels[0][:4]
 leds_labels = legs_labels[1][4:]
 leds_handles = legs_labels[0][4:]
 
-
 leg1 = plt.legend(opsin_handles, opsin_labels, title = "Opsins", bbox_to_anchor=(1.2, .9), loc=0)
-leg2 = plt.legend(leds_handles, leds_labels, title = "LEDs", bbox_to_anchor=(1.036, .09), loc=0)
+leg2 = plt.legend(leds_handles, leds_labels, title = "LEDs", bbox_to_anchor=(1.23, .5), loc=0)
 ax.add_artist(leg1)
+plt.tight_layout()
+
 plt.show()
